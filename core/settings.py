208752892a6ebf5dddb3ba7da8ba701f421de22f
Neto Gomes <124839031+netoADS@users.noreply.github.com>
2024-02-27 21:32:26 -0300

    Add files via upload

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ee^63c$&ibhb2dxhrcv@w^1$n(ry-96yq#_a!l9n0fe(1d4ij3'
+SECRET_KEY = 'django-insecure-y&2#3167$lj-r(l5j!s08y#*n^1^lv&@2=wo&cgcdyjwq*sltl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('PRODUCTION_ENVIRONMENT', '').lower() != 'true'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'HOST_ADDRESS',
+    'localhost',
+    '127.0.0.1'
+]
 
 
 # Application definition
@@ -73,12 +79,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+if not DEBUG:
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'db.sqlite3',
+        }}
 
 
 # Password validation
